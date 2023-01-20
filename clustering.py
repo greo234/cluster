fc4a37da835369378f10a7b1d3f7c37546cec201
greo234 <117023945+greo234@users.noreply.github.com>
2023-01-20 06:20:13 +0000

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -2,7 +2,7 @@
 """
 Created on Wed Jan 18 13:58:59 2023
 
-@author: matah
+@author: Omoregbe Olotu
 """
 import numpy as np
 import pandas as pd
@@ -13,9 +13,12 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 
+#filefolders
 indicator = "gdppercapita.xls"
+file = 'TotalPopulation.xls'
 
-#reading excel file with the required years 1981 and 2021
+
+#reading excel file with the required years 1981 and 2021 for clustering
 df = pd.read_excel(indicator, skiprows=3,
                            usecols=[0,25,65])
 #drop missing rows
@@ -25,11 +28,15 @@ df = df.reset_index(drop = True)
 print(df)
 
 #visualising data
-#df.plot('1981', '2021', kind = 'scatter', color = 'blue')
-#plt.title('GDP per Capita')
+df.plot('1981', '2021', kind = 'scatter', color = 'blue')
+plt.title('GDP per Capita')
 
 #new dataframe for clustering
 df_clust = df[['1981','2021']].copy()
+
+#data in array format
+c = df[['1981', '2021']].values
+print(c)
 
 
 # Normalize the data
@@ -37,8 +44,11 @@ max_value = df_clust.max()
 min_value = df_clust.min()
 #normalized data
 df_norm = (df_clust - min_value) / (max_value - min_value)
-print(df_norm)
 
+#converting to array
+n = df_norm.values
+
+#getting the sse for elbow method to find the number of clusters to use
 sse = []
 for i in range(1, 11):
     kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
@@ -72,7 +82,10 @@ print(f'Silhouette Score: {silhouette_score(df_norm, labels)}')
 cen = kmeans.cluster_centers_
 print(cen)
 
-cen = pd.DataFrame(cen, columns=['1981','2021'])
+#converting to dataframe
+cent = pd.DataFrame(cen, columns=['1981','2021'])
+print(cent)
+print(cent)
 
 
 # Plot the scatter plot of the clusters
@@ -90,19 +103,172 @@ print(y_predict)
 
 #creating new dataframe with the labels for each country
 df['cluster'] = y_predict
-print(df)
+df_norm['cluster'] = y_predict
+print('\n Countries and Cluster Category:\n', df)
 
 
-# plot using the labels to select colour
-plt.figure(figsize=(10.0, 10.0))
-col = ["tab:blue", "tab:orange", "tab:green", "tab:red", "tab:purple", "tab:brown"]
-for l in range(ncluster): # loop over the different labels
-    plt.plot(df_norm[labels==l]["1981"], df_norm[labels==l]["2021"], "o", markersize=3, color=col[l])
-#
-# show cluster centres
-for ib in range(ncluster):
-    xb, yb = cen[ib,:]
-    plt.plot(xb, yb, "dk", markersize=10)
-    plt.xlabel("1981")
-    plt.ylabel("2021")
-    plt.show()
+#plotting the normalised GDPshowing the cluster centres
+plt.figure()
+plt.scatter(n[y_predict == 0, 0], n[y_predict == 0, 1], s = 50, c = 'blue',label = 'cluster 0')
+plt.scatter(n[y_predict == 1, 0], n[y_predict == 1, 1], s = 50, c = 'red',label = 'cluster 1')
+plt.scatter(n[y_predict == 2, 0], n[y_predict == 2, 1], s = 50, c = 'green',label = 'cluster 2')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:,1], s = 50, c = 'black', label = 'Centroids')
+plt.title('GDP/Capita (Normalised)')
+plt.xlabel('1981')
+plt.ylabel('2021')
+plt.legend()
+plt.show()
+
+
+# Logistic function forecasting
+def logistic(t, n0, g, t0):
+    """ This fuction calculates the logistic function with scale factor n0
+    and growth rate g"""
+    f = n0 / (1 + np.exp(-g*(t - t0)))
+    return f
+
+
+
+def read(filename):
+    """ This function reads a file in its original and transponsed format"""
+    data = pd.read_excel(filename,skiprows=3)
+    data.drop(data.columns[[1,2,3]], axis=1, inplace=True)
+    return data, data.transpose()
+    
+
+#reading the Total population file from the world bank format
+pop, pop2 = read(file)
+print(pop2)
+
+#rename the columns
+df2 = pop2.rename(columns=pop2.iloc[0])
+
+#drop the country name
+df2 = df2.drop(index=df2.index[0], axis=0)
+df2['Year'] = df2.index
+
+print(df2)
+
+#fitting the data
+df_fit = df2[['Year', 'China']].apply(pd.to_numeric, 
+                                               errors='coerce')
+
+#fits the logistic data
+param, covar = opt.curve_fit(logistic, df_fit['Year'], df_fit['China'], 
+                             p0=(3e12, 0.03, 2000))
+
+sigma = np.sqrt(np.diag(covar))
+
+df_fit['fit'] = logistic(df_fit['Year'], *param)
+
+# Plot the forecast
+plt.plot(df_fit["Year"], df_fit["China"], 
+         label="Population")
+plt.plot(df_fit["Year"], df_fit["fit"], label="fit")
+plt.xlabel("Year")
+plt.ylabel("Population")
+plt.legend()
+plt.title('CHINA')
+plt.show()
+
+
+# Forecast for the next 10 years
+year = np.arange(1960, 2031)
+forecast = logistic(year, *param)
+
+plt.plot(df_fit["Year"], df_fit["China"], 
+         label="Population")
+plt.plot(year, forecast, label="Forecast")
+plt.xlabel("Year")
+plt.ylabel("Population")
+plt.legend()
+plt.title('CHINA')
+plt.show()
+
+# Error ranges calculation
+def err_ranges(x, func, param, sigma):
+    """
+    This function calculates the upper and lower limits of function, parameters and
+    sigmas for a single value or array x. The function values are calculated for 
+    all combinations of +/- sigma and the minimum and maximum are determined.
+    This can be used for all number of parameters and sigmas >=1.
+    """
+
+    import itertools as iter
+    
+    # initiate arrays for lower and upper limits
+    lower = func(x, *param)
+    upper = lower
+    
+    # Create a list of tuples of upper and lower limits for parameters
+    uplow = []   
+    for p,s in zip(param, sigma):
+        pmin = p - s
+        pmax = p + s
+        uplow.append((pmin, pmax))
+        
+    pmix = list(iter.product(*uplow))
+    
+    # calculate the upper and lower limits
+    for p in pmix:
+        y = func(x, *p)
+        lower = np.minimum(lower, y)
+        upper = np.maximum(upper, y)
+        
+    return lower, upper
+
+# calculates the error ranges 
+low, up = err_ranges(year, logistic, param, sigma)
+
+#plotting China's Total Population
+plt.figure(dpi=140)
+plt.plot(df_fit["Year"], df_fit["China"], 
+         label="Population")
+plt.plot(year, forecast, label="forecast")
+plt.fill_between(year, low, up, color="yellow", alpha=0.7)
+plt.xlabel("Year")
+plt.ylabel("Population")
+plt.legend()
+plt.title('China Population Forecast - Confidence Interval')
+plt.show()
+
+#prints the error ranges
+print(err_ranges(2031, logistic, param, sigma))
+
+
+#fitting the United States data
+df_US = df2[['Year', 'United States']].apply(pd.to_numeric, 
+                                               errors='coerce')
+#fits the logistic data
+param, covar = opt.curve_fit(logistic, df_US['Year'], df_US['United States'], 
+                             p0=(3e12, 0.03, 2000))
+
+sigma = np.sqrt(np.diag(covar))
+
+df_US['fit'] = logistic(df_US['Year'], *param)
+
+# Plot the forecast
+plt.plot(df_US["Year"], df_US["United States"], 
+         label="Population")
+plt.plot(df_US["Year"], df_US["fit"], label="fit")
+plt.xlabel("Year")
+plt.ylabel("Population")
+plt.legend()
+plt.title('United States')
+plt.show()
+
+
+# Forecast for the next 10 years
+year = np.arange(1960, 2031)
+forecast = logistic(year, *param)
+
+plt.plot(df_US["Year"], df_US["United States"], 
+         label="Population")
+plt.plot(year, forecast, label="Forecast")
+plt.xlabel("Year")
+plt.ylabel("Population")
+plt.legend()
+plt.title('United States')
+plt.show()
+
+
